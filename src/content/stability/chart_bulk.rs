215621d508e653123d896f17cbcef245ce9a4971
Konstantin Antonov <knstantna@gmail.com>
2025-03-27 17:41:43 +0300

    Back | fix bulk_chart

--- src/content/stability/chart_bulk.rs
+++ src/content/stability/chart_bulk.rs
@@ -87,6 +87,18 @@ impl ChartBulk {
             curve_lever.clone(),
             &RGBColor(150, 0, 0),
         ))?;
+        // отрисовка линии угла макс. разницы
+        let curve_dif = [(self.b.0, self.p_40_dso), (self.b.0, 0.)];
+        chart.draw_series(LineSeries::new(
+            curve_dif.clone(),
+            &RGBColor(150, 150, 0),
+        ))?;
+        // отрисовка вертикальной линии к первой точке пересечения ДСО и кривой кренящих плеч
+        let curve_theta_g = [(self.p1_dso.0, 0.), (self.p1_dso.0, self.p1_dso.1)];
+        chart.draw_series(LineSeries::new(
+            curve_theta_g.clone(),
+            &RGBColor(150, 150, 0),
+        ))?;
         // отрисовка точек кривой плеч кренящего момента
         chart.draw_series(PointSeries::of_element(
             [curve_lever[1], curve_lever[2]],
@@ -109,15 +121,9 @@ impl ChartBulk {
                 + Text::new(format!("{:.3}", c.1), (5, -15), ("sans-serif", 14).into_font());
             },
         ))?;
-        // отрисовка линии угла макс. разницы
-        let curve_dif = [(self.b.0, self.p_40_dso)];
-        chart.draw_series(LineSeries::new(
-            curve_dif.clone(),
-            &RGBColor(150, 150, 0),
-        ))?;
         // отрисовка точек угла макс. разницы
         chart.draw_series(PointSeries::of_element(
-            curve_dif.clone(),
+            [curve_dif[0]],
             3,
             &RGBColor(150, 0, 0),
             &|c, s, st| {
@@ -127,20 +133,14 @@ impl ChartBulk {
             },
         ))?;
         chart.draw_series(PointSeries::of_element(
-            curve_dif.clone(),
+            [curve_dif[1]],
             3,
             &RGBColor(150, 0, 0),
             &|c, s, st| {
                 return EmptyElement::at(c)  
                 + Circle::new((0,0),s,st.filled()) 
-                + Text::new(format!("{:.3}:{:.3}", c.0, c.1), (10, 0), ("sans-serif", 14).into_font());
+                + Text::new(format!("{:.3}", c.0), (5, -15), ("sans-serif", 14).into_font());
             },
-        ))?;
-        // отрисовка вертикальной линии к первой точке пересечения ДСО и кривой кренящих плеч
-        let curve_theta_g = [(self.p1_dso.0, 0.), (self.p1_dso.0, self.p1_dso.1)];
-        chart.draw_series(LineSeries::new(
-            curve_theta_g.clone(),
-            &RGBColor(150, 150, 0),
         ))?;
         // отрисовка точки на оси Х, надпись выше линии
         chart.draw_series(PointSeries::of_element(
@@ -153,23 +153,22 @@ impl ChartBulk {
                 + Text::new(format!("θg = {:.3}", c.0), (5, -15), ("sans-serif", 14).into_font());
             },
         ))?;
-
         // надпись с площадью по центру заливки
         let delta_x = self.b.0 - self.p1_dso.0;
         let y_max_area = self.dso.iter().filter(|v| v.0 <= self.b.0 ).fold(f64::MIN, |r, v| r.max(v.1) );
         let y_max_dso = self.dso.iter().fold(f64::MIN, |r, v| r.max(v.1) );
-        let x_shift = (y_max_area/y_max_dso).min(1.5).max(1.);
-        let x_mid = self.p1_dso.0 + delta_x * x_shift / 2.;
-        let y_max = y_max_area.max(y_max_dso);
-        let delta_y = y_max - self.b.1;
-        let y_mid = self.b.1 + delta_y/2.;
+        let shift = (y_max_dso/y_max_area).min(1.25).max(1.);
+        let shift = shift*shift;
+        let x_mid = self.p1_dso.0 + delta_x * shift / 2.;
+        let delta_y = y_max_area - self.b.1;
+        let y_mid = self.b.1 + delta_y / (2. * shift);
         chart.draw_series(PointSeries::of_element(
             [(x_mid, y_mid)],
             3,
             &RGBColor(0, 0, 0),
             &|c, _, _| {
                 return EmptyElement::at(c)  
-                + Text::new(format!("S={:.3}", self.area), (0, -7), ("sans-serif", 14).into_font());
+                + Text::new(format!("S={:.3}", self.area), (-30, -7), ("sans-serif", 14).into_font());
             },
         ))?;
         // заливка области
