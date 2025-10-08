7d91236fb671ceb514e07772003d36fe88773da0
KnstAnt <knstantna@gmail.com>
2025-10-08 18:16:35 +0300

    Back | update sql for voyage

--- src/db/api_server.rs
+++ src/db/api_server.rs
@@ -502,6 +502,45 @@ impl ApiServer {
                     i.icing_type as icing, \
                     a.name AS area, \
                     v.description AS description, \
+                    llt.name as load_line 
+                FROM 
+                    voyage as v
+                JOIN 
+                    ship_icing AS i ON v.icing_type_id = i.id
+                JOIN 
+                    ship_water_area AS a ON v.water_area_id = a.id
+                JOIN ship_available_load_line_types AS sallt ON
+                    sallt.ship_id = v.ship_id AND
+                    sallt.project_id IS NOT DISTINCT FROM v.project_id
+                JOIN load_line_type AS llt ON
+                    sallt.load_line_type_id = llt.id
+                WHERE 
+                    sallt.is_active IS TRUE AND
+                    v.ship_id={} AND 
+                    v.project_id IS NOT DISTINCT FROM {}
+                LIMIT 1;",
+                    self.ship_id, 
+                    self.project_id,
+                ))
+                .map_err(|e| Error::FromString(format!("api_server get_voyage error: {e}")))?,
+        )
+        .map_err(|e| Error::FromString(format!("api_server get_voyage error: {e}")))?
+        .data()
+        .ok_or(Error::FromString(format!(
+            "api_server get_voyage error: no data!"
+        )))
+    }
+/*    pub fn get_voyage(&mut self) -> Result<VoyageData, Error> {
+        VoyageDataArray::parse(
+            &self
+                .fetch(&format!(
+                    "SELECT
+                    v.code as code, \
+                    v.density as density, \
+                    v.wetting_timber as wetting, \
+                    i.icing_type as icing, \
+                    a.name AS area, \
+                    v.description AS description, \
                     c.{} as load_line 
                 FROM 
                     voyage as v
@@ -534,6 +573,7 @@ impl ApiServer {
             "api_server get_voyage error: no data!"
         )))
     }
+*/
     //
     pub fn get_itinerary(&mut self) -> Result<ItineraryDataArray, Error> {
         ItineraryDataArray::parse(
